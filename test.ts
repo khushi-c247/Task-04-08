bfa8e89ccaf87a15b5d10f13a884927414554279
khushi-c247 <141140921+khushi-c247@users.noreply.github.com>
2023-08-05 12:05:29 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
 var num : number = 8;
 var flag: boolean= true
-
+//find Cube or not 
 // for (let i = 1; i < num; i++)
 // {
 //     if (i*i*i == num)
@@ -14,31 +14,10 @@ var flag: boolean= true
 // if (flag)
 //     console.log("not a qube");
 
+//find Cube or not using Mathpow method
    var n : number = 1;
     if(Math.pow(n,3) == num)
     {
         console.log(n);
        
     }
-
-
-// function qubeOrnot ( num : number ) {
-//     var ans = 1
-//     var flag = true;
-//     if( ans*ans*ans == num)
-//     {
-//         flag = false;
-//         return ans;
-//     }
-//     else 
-//     {
-//         ans= ans+1
-//         qubeOrnot(num)
-//     }
-//     if(flag)
-//     {
-//         return 0;
-//     }
-// }
-
-// console.log(qubeOrnot(27));
